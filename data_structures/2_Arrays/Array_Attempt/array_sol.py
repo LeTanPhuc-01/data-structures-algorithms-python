160936b3f46160c6a226d78e8371eba58cdd5a4c
LeTanPhuc-01 <152056236+LeTanPhuc-01@users.noreply.github.com>
2024-07-10 22:30:34 +0000

    Wed, Jul 10, 2024, 5:30 PM -05:00

--- data_structures/2_Arrays/Array_Attempt/array_sol.py
+++ data_structures/2_Arrays/Array_Attempt/array_sol.py
@@ -2,3 +2,8 @@ exp = [2200, 2350, 2600, 2130, 2190]
 
 print(f"Dollars spent extra in Feb compared to Jan: {exp[1] - exp[0]}")
 print(f"First quarter expense: {sum(exp[0:3])}")
+print("Did I spend 2000 exactly?", 2000 in exp)
+exp.append(1980)
+exp[4] = exp[4] - 200
+print(exp)
+
