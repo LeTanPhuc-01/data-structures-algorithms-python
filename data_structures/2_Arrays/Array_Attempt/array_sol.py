f481e9daa291d8e41b9cac287fb0dad201a81a10
LeTanPhuc-01 <152056236+LeTanPhuc-01@users.noreply.github.com>
2024-07-10 22:38:01 +0000

    Wed, Jul 10, 2024, 5:38 PM -05:00

--- data_structures/2_Arrays/Array_Attempt/array_sol.py
+++ data_structures/2_Arrays/Array_Attempt/array_sol.py
@@ -5,11 +5,14 @@ print(f"Dollars spent extra in Feb compared to Jan: {exp[1] - exp[0]}")
 print(f"First quarter expense: {sum(exp[0:3])}")
 # 3. Find out if you spent exactly 2000 dollars in any month
 print("Did I spend 2000 exactly?", 2000 in exp)
-4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
+#4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
 exp.append(1980)
 # 5. You returned an item that you bought in a month of April and got a refund of 200$. Make a correction to your monthly expense list
-exp[4] = exp[4] - 200
+exp[3] = exp[3] - 200
 
 print(exp)
 
 heros=['spider man','thor','hulk','iron man','captain america']
+print(heros.len())
+heros.append("black panther")
+print(heros)
